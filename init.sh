204f94ecc559b60e2949f0150163ad93e0e59b92
Vallabh Kansagara <vrkansagara@gmail.com>
2023-02-19 01:44:55 +0530

    adding simple cursor/color picker and dwmblocks changes
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
-ps -ef | grep "dwmblocks" | grep -v grep | awk "{print \$2}" | xargs --no-run-if-empty sudo kill -9
+ps -ef | grep "dwmblocks" | grep -v grep | awk "{print \$2}" | xargs --no-run-if-empty sudo kill -9jj
 /usr/local/bin/dwmblocks&%
+
+# Required sxcs
+sudo apt install libxcursor-dev
--- submodule.sh
+++ submodule.sh
@@ -8,6 +8,7 @@ git submodule add -f git://git.suckless.org/dmenu vendor/dmenu
 git submodule add -f git://git.suckless.org/st vendor/st
 git submodule add -f git://git.suckless.org/slock vendor/slock
 git submodule add -f git://git.suckless.org/scroll vendor/scroll
+git submodule add -f https://github.com/N-R-K/sxcs vendor/sxcs
 
 git submodule foreach git reset --hard HEAD
 git submodule foreach git clean -fd
