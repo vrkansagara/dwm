9909a3437367c4644a49b8435f36dac7fdc752db
Vallabh Kansagara <vrkansagara@gmail.com>
2023-02-20 10:37:50 +0530

    removal of dwmblock histor
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- init.sh
+++ init.sh
@@ -6,13 +6,11 @@ sudo apt install libxcursor-dev
 
 #sudo apt install  fonts-noto-color-emoji
 #mkdir -p ~/.fonts/NotoEmoji
-curl -L 'https://raw.githubusercontent.com/googlefonts/noto-emoji/main/fonts/NotoColorEmoji.ttf' -o ~/.fonts/NotoEmoji/NotoColorEmoji.ttf
+#curl -L 'https://raw.githubusercontent.com/googlefonts/noto-emoji/main/fonts/NotoColorEmoji.ttf' -o ~/.fonts/NotoEmoji/NotoColorEmoji.ttf
 #fc-cache -fv
 
 sudo apt remove --purge fonts-noto-color-emoji unifont
 
- 
 sudo fc-cache -fv
 fc-list | grep -i emoji
-
 echo "🩷💀🫱"
